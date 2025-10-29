97fdeb9bb8ca3055ec4346c4f4808267aa7034bc
Srivastava <anshika.srivastava@dxc.com>
2025-10-29 17:05:47 +0530

    FIX: Use env variable for DB URI instead of SQLite

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 # SQLite database configure
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
 
